2fc72b418924ea32d80dbe47c71ca3ede51e6cbe
Your Name <youremail@example.com>
2026-01-08 19:50:37 +0300

    Remove invalid UserProfileView import

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserProfileView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user-register'),
     path('login/', LoginView.as_view(), name='user-login'),
-    path('profile/', UserProfileView.as_view(), name='user-profile'),
 ]
