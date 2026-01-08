3f6e0b982cad6cd137d763d0f54affed24bcbb23
Your Name <youremail@example.com>
2026-01-08 19:24:51 +0300

    Fix auth view imports in urls

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserProfileView
+from .views import RegisterView, LoginView, UserProfileView
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('register/', RegisterView.as_view(), name='user-register'),
+    path('login/', LoginView.as_view(), name='user-login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
 ]
