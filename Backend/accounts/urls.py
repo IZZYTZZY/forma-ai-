0984965311203b331286621d584b74f374fd68b4
Your Name <youremail@example.com>
2026-01-08 21:06:25 +0300

    Fix auth API routing

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='user-register'),
-    path('login/', LoginView.as_view(), name='user-login'),
+    path("register/", RegisterView.as_view(), name="user-register"),
+    path("login/", LoginView.as_view(), name="user-login"),
 ]
